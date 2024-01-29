0f0be19aaf4b40dd72b6cdce8d1ef84960d6f01d
Wesley de Oliveira <wesley.oliveirabd@gmail.com>
2024-01-29 15:34:41 -0300

    Inserindo arquivo principal.js

---
+++ principal.js
@@ -0,0 +1,10 @@
+var banner = ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+
+var bannerAtual = 0;
+
+function trocaBanner() {
+    bannerAtual = (bannerAtual + 1) %2;
+    document.querySelector('h2#mensagem').textContent = banner[bannerAtual];
+}
+
+setInterval(trocaBanner, 2000);
